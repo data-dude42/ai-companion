626239cca707f442b938b1ae7eb33775d17c6bd9
data-dude42 <ss8820788@gmail.com>
2024-10-25 16:52:15 -0400

    added sidebar.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import {Navbar} from '@/components/navbar'
+import { Sidebar } from 'lucide-react';
 const RootLayout = ({
     children
 }:{
@@ -7,6 +8,10 @@ const RootLayout = ({
     return (
         <div className="h-full">
             <Navbar />
+            <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+                <Sidebar/>
+
+            </div>
             <main className="md:pl-20 pt-16 h-full">
             {children}
             </main>
