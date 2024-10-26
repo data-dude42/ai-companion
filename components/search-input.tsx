c22feed84e6de2006a9cb157f706c6c605b92c9b
data-dude42 <ss8820788@gmail.com>
2024-10-26 12:03:11 -0400

    updated search-input.tsx and updated use-debounce.tsx

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,9 +1,22 @@
 "use client";
-
+import {useState} from "react"
 import {Search} from "lucide-react";
 import {Input} from "@/components/ui/input";
+import{useRouter, useSearchParams} from "next/navigation";
 
 export const SearchInput = () => {
+
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const categoryId = searchParams.get('categoryId');
+    const name = searchParams.get("name")
+
+    const [value, setValue] = useState(name || "");
+
+
+
+
     return (
         <div className="relative">
             <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
