8064dd8728bad0b3d9e977a7ecce16e2b6e6fcb7
data-dude42 <ss8820788@gmail.com>
2024-10-26 11:51:47 -0400

    updaed search-input.tsx

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import {Search} from "lucide-react";
+import {Input} from "@/components/ui/input";
 export const SearchInput = () => {
     return (
-        <div>
-            Search Input!
+        <div className="relative">
+            <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+            <Input
+             placeholder="search..."
+             className="pl-10 bg-primary/10"
+             />
         </div>
     )
 }
